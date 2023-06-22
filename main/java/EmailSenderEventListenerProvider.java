938273588271c2de62421bbdb7e44c1013ca1aff
PMontoliuRico <100042312+PMontoliuRico@users.noreply.github.com>
2023-06-22 14:24:41 +0200

    New Event Listener
    Event listener coded to send a modifiable email to the desired email on new user registrations.

---
+++ main/java/EmailSenderEventListenerProvider.java
@@ -0,0 +1,52 @@
+import org.keycloak.events.Event;
+import org.keycloak.events.EventListenerProvider;
+import org.keycloak.events.EventType;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserModel;
+import org.keycloak.email.DefaultEmailSenderProvider;
+import org.keycloak.email.EmailException;
+
+public class EmailSenderEventListenerProvider implements EventListenerProvider {
+
+    private final KeycloakSession session;
+
+    public EmailSenderEventListenerProvider(KeycloakSession session) {
+        this.session = session;
+    }
+
+    @Override
+    public void onEvent(Event event) {
+        if (event.getType() == EventType.REGISTER) {
+            
+            RealmModel realm = this.model.getRealm(event.getRealmId());
+            UserModel newRegisteredUser = this.session.users().getUserById(realm, event.getUserId());
+
+            String emailPlainContent = "New user registration\n\n" +
+                "Email: " + newRegisteredUser.getEmail() + "\n" +
+                "Username: " + newRegisteredUser.getUsername() + "\n" +
+                "Client: " + event.getClientId();
+
+            String emailHtmlContent = "<h1>New user registration</h1>" +
+                "<ul>" +
+                "<li>Email: " + newRegisteredUser.getEmail() + "</li>" +
+                "<li>Username: " + newRegisteredUser.getUsername() + "</li>" +
+                "<li>Client: " + event.getClientId() + "</li>" +
+                "</ul>";
+
+            DefaultEmailSenderProvider senderProvider = new DefaultEmailSenderProvider(session);
+
+            try {
+                senderProvider.send(session.getContext().getRealm().getSmtpConfig(), "admin@example.com", "Keycloak - New Registration", emailPlainContent, emailHtmlContent);
+            } catch (EmailException e) {
+                log.error("Failed to send email", e);
+            }
+            
+        }
+    }
+
+    @Override
+    public void close() {
+        // Método requerido por la interfaz, pero que no necesitamos implementar
+    }
+}
---
+++ main/java/EmailSenderEventListenerProviderFactory.java
@@ -0,0 +1,40 @@
+import org.keycloak.Config;
+import org.keycloak.events.EventListenerProvider;
+import org.keycloak.events.EventListenerProviderFactory;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+
+public class EmailSenderEventListenerProviderFactory implements EventListenerProviderFactory {
+
+    @Override
+    public EventListenerProvider create(KeycloakSession session) {
+        return new EmailSenderEventListenerProvider(session);
+    }
+
+    @Override
+    public void init(Config.Scope config) {
+        // Método requerido por la interfaz, pero que no necesitamos implementar
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory factory) {
+        // Método requerido por la interfaz, pero que no necesitamos implementar
+    }
+
+    @Override
+    public void close() {
+        // Método requerido por la interfaz, pero que no necesitamos implementar
+    }
+
+    @Override
+    public String getId() {
+        return "emailSenderEventListener";
+        /**
+         * The getId() method is used to provide a unique identifier for the custom event listener provider factory.
+         * This identifier is used within Keycloak to reference and lookup the correct factory when instances of the
+         * EventListenerProvider interface are needed.
+         * When an event occurs within Keycloak that needs to be broadcasted to listeners, Keycloak looks up the 
+         * appropriate listener based on the ID returned by getId()
+         */
+    }
+}
