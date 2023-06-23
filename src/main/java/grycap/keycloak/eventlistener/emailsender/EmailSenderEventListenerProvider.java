88124a2732661fdb2909808eddf12d8516583a93
PMontoliuRico <100042312+PMontoliuRico@users.noreply.github.com>
2023-06-23 13:41:54 +0200

    New send method with UserModel
    Changing the String parameter on .send() by a UserModel and allowing multiple admins.

--- src/main/java/grycap/keycloak/eventlistener/emailsender/EmailSenderEventListenerProvider.java
+++ src/main/java/grycap/keycloak/eventlistener/emailsender/EmailSenderEventListenerProvider.java
@@ -3,52 +3,136 @@ package grycap.keycloak.eventlistener.emailsender;
 import org.keycloak.events.Event;
 import org.keycloak.events.EventListenerProvider;
 import org.keycloak.events.EventType;
+import org.keycloak.events.admin.AdminEvent;
+import org.keycloak.events.admin.ResourceType;
+import org.keycloak.forms.login.LoginFormsProvider;
+import org.keycloak.models.ClientModel;
+import org.keycloak.models.GroupModel;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
+import org.keycloak.models.RealmProvider;
+import org.keycloak.models.RoleModel;
 import org.keycloak.models.UserModel;
+import org.keycloak.provider.ServerInfoAwareProviderFactory;
+import org.keycloak.storage.adapter.InMemoryUserAdapter;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
+import org.jboss.logging.Logger;
+import org.keycloak.authentication.FormAction;
+import org.keycloak.authentication.FormActionFactory;
+import org.keycloak.authentication.FormContext;
+import org.keycloak.authentication.ValidationContext;
 import org.keycloak.email.DefaultEmailSenderProvider;
 import org.keycloak.email.EmailException;
+import org.keycloak.email.EmailSenderProvider;
 
-public class EmailSenderEventListenerProvider implements EventListenerProvider {
+public class EmailSenderEventListenerProvider implements EventListenerProvider, FormAction {
 
     private final KeycloakSession session;
+//  private static final Logger log = Logger.getLogger(EmailSenderEventListenerProviderFactory.class);
+    private static final Logger LOG = Logger.getLogger(EmailSenderEventListenerProvider.class);
 
-    public EmailSenderEventListenerProvider(KeycloakSession session) {
+    private Set<String> groups;
+    private ResourceType target_event;
+    private String kubeauthorizer_endpoint, /*email_subject*/kubeauthorizer_token, kubeauthorizer_userclaim;
+//   private String[] email_receivers;
+    private RealmProvider model;
+    private Set<String> target_groups;
+
+    public EmailSenderEventListenerProvider(KeycloakSession session, ResourceType event, Set<String> groups, String kubeauthorizer_endpoint, String kubeauthorizer_token, String kubeauthorizer_userclaim/*, String email_subject, String[] email_receivers*/)  {
         this.session = session;
-    }
-
-    @Override
-    public void onEvent(Event event) {
-        if (event.getType() == EventType.REGISTER) {
-            
-            RealmModel realm = this.model.getRealm(event.getRealmId());
-            UserModel newRegisteredUser = this.session.users().getUserById(realm, event.getUserId());
-
-            String emailPlainContent = "New user registration\n\n" +
-                "Email: " + newRegisteredUser.getEmail() + "\n" +
-                "Username: " + newRegisteredUser.getUsername() + "\n" +
-                "Client: " + event.getClientId();
-
-            String emailHtmlContent = "<h1>New user registration</h1>" +
-                "<ul>" +
-                "<li>Email: " + newRegisteredUser.getEmail() + "</li>" +
-                "<li>Username: " + newRegisteredUser.getUsername() + "</li>" +
-                "<li>Client: " + event.getClientId() + "</li>" +
-                "</ul>";
-
-            DefaultEmailSenderProvider senderProvider = new DefaultEmailSenderProvider(session);
-
-            try {
-                senderProvider.send(session.getContext().getRealm().getSmtpConfig(), "admin@example.com", "Keycloak - New Registration", emailPlainContent, emailHtmlContent);
-            } catch (EmailException e) {
-                log.error("Failed to send email", e);
-            }
-            
-        }
+        this.model = session.realms();
+        this.target_event = event;
+        this.target_groups = groups;
+        this.kubeauthorizer_endpoint = kubeauthorizer_endpoint;
+        this.kubeauthorizer_token = kubeauthorizer_token;
+        this.kubeauthorizer_userclaim = kubeauthorizer_userclaim;
+    //    this.email_subject = email_subject;
+    //    this.email_receivers = email_receivers;
     }
 
     @Override
     public void close() {
         // Método requerido por la interfaz, pero que no necesitamos implementar
+    }
+
+    @Override
+    public void success(FormContext context) {
+        UserModel user = context.getUser();
+        RealmModel realm = context.getRealm();
+        
+        // List of admin emails
+        List<String> adminEmails = Arrays.asList("admin@example.com", "adm@ex.com");
+
+        for (String emailAdmin : adminEmails) {
+            UserModel admin = new InMemoryUserAdapter(context.getSession(), realm, UUID.randomUUID().toString());
+            admin.setEmail(emailAdmin);
+            LOG.info("EmailFormActionFactory success, sent email to " + emailAdmin + " mentioning that " + user.getEmail() + " has registered!" );
+            EmailSenderProvider emailSender = context.getSession().getProvider(EmailSenderProvider.class);
+            try {
+                emailSender.send(realm.getSmtpConfig(), admin, "Self Registration with Keycloak", "Hi Admin, a new user with the email "
+                                + user.getEmail() + " has just registered with keycloak! " +
+                                "This is an automatic notice.",
+                                "<h3>Hi Admin,</h3>" +
+                                "<p>a new user with the email " + user.getEmail() + " has just registered with keycloak! </p>" +
+                                "<p>This is an automatic notice." );
+            } catch (EmailException e) {
+                LOG.error("EmailFormActionFactory success, could not send notification to admin", e);
+            }
+        }
+    }
+
+
+    @Override
+    public void buildPage(FormContext context, LoginFormsProvider form) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buildPage'");
+    }
+
+
+    @Override
+    public void validate(ValidationContext context) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'validate'");
+    }
+
+
+    @Override
+    public boolean requiresUser() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'requiresUser'");
+    }
+
+
+    @Override
+    public boolean configuredFor(KeycloakSession session, RealmModel realm, UserModel user) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'configuredFor'");
+    }
+
+
+    @Override
+    public void setRequiredActions(KeycloakSession session, RealmModel realm, UserModel user) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setRequiredActions'");
+    }
+
+
+    @Override
+    public void onEvent(Event event) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'onEvent'");
+    }
+
+
+    @Override
+    public void onEvent(AdminEvent event, boolean includeRepresentation) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'onEvent'");
     }
 }
--- src/main/java/grycap/keycloak/eventlistener/emailsender/EmailSenderEventListenerProviderFactory.java
+++ src/main/java/grycap/keycloak/eventlistener/emailsender/EmailSenderEventListenerProviderFactory.java
@@ -3,19 +3,63 @@ package grycap.keycloak.eventlistener.emailsender;
 import org.keycloak.Config;
 import org.keycloak.events.EventListenerProvider;
 import org.keycloak.events.EventListenerProviderFactory;
+import org.keycloak.events.admin.ResourceType;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.provider.ServerInfoAwareProviderFactory;
 
-public class EmailSenderEventListenerProviderFactory implements EventListenerProviderFactory {
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
 
+import org.jboss.logging.Logger;
+
+public class EmailSenderEventListenerProviderFactory implements EventListenerProviderFactory, ServerInfoAwareProviderFactory {
+
+    private static final Logger log = Logger.getLogger(EmailSenderEventListenerProviderFactory.class);
+    private Set<String> groups;
+    private ResourceType target_event;
+    private String kubeauthorizer_endpoint, /*email_subject*/kubeauthorizer_token, kubeauthorizer_userclaim;
+//    private String[] email_receivers;
     @Override
     public EventListenerProvider create(KeycloakSession session) {
-        return new EmailSenderEventListenerProvider(session);
+        return new EmailSenderEventListenerProvider(session, this.target_event, this.groups, this.kubeauthorizer_endpoint, this.kubeauthorizer_token, this.kubeauthorizer_userclaim);
     }
 
     @Override
     public void init(Config.Scope config) {
-        // Método requerido por la interfaz, pero que no necesitamos implementar
+        groups = new HashSet<>();
+        log.info( String.format( "### ------------  %s.init() ------------ ###", this.getId() ) );
+        
+        String[] group_names = config.get("OIDCGroups","").split(",");
+        if (group_names != null) {
+            for (String name : group_names) {
+                this.groups.add(name);
+            }
+        }
+
+        // this.email_receivers = config.get("email_receivers", "").split(",");
+
+        this.target_event = ResourceType.valueOf(config.get("adminEvent", "GROUP_MEMBERSHIP" ) );
+        this.kubeauthorizer_endpoint = config.get("kubeAuthorizerEndpoint", "");
+        this.kubeauthorizer_token = config.get("kubeAuthorizerToken", "");
+        this.kubeauthorizer_userclaim = config.get("kubeAuthorizerUserClaim", "username");
+        // this.email_subject = config.get("emailSubject", "Default Subject");
+        log.info ("Configuration variables: ");
+        log.info ("\tOIDC groups: " + this.groups.toString());
+        log.info ("\tAdmin event target: " + this.target_event.toString());
+        log.info ("\tKube-authorizer endpoint: " + this.kubeauthorizer_endpoint );
+        log.info ("\tKube-authorizer token: " + this.kubeauthorizer_token);
+        log.info ("\tKube-authorizer username claim [sub,username] : " + this.kubeauthorizer_userclaim );
+/*         log.info ("\tEmail subject: " + this.email_subject);
+        for (String email : email_receivers) {
+            log.info("\tEmail receiver: " + email);
+}
+        
+*/
+        
+        log.info( String.format("-----------------------------------------------------------") );
     }
 
     @Override
@@ -38,5 +82,14 @@ public class EmailSenderEventListenerProviderFactory implements EventListenerPro
          * When an event occurs within Keycloak that needs to be broadcasted to listeners, Keycloak looks up the 
          * appropriate listener based on the ID returned by getId()
          */
+    }
+    @Override
+    public Map<String, String> getOperationalInfo() {
+        Map<String, String> ret = new LinkedHashMap<>();
+        ret.put("OIDCgroups", this.groups.toString());
+        ret.put("AdminEventTarget", this.target_event.toString());
+        ret.put("KubeauthorizerEndpoint", this.kubeauthorizer_endpoint);
+        ret.put("KubeauthorizerUserClaim", this.kubeauthorizer_userclaim);
+        return ret;
     }
 }
